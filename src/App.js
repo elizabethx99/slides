aaed8166228c4a87f9c69bea2277fd8b242e25ae
Elizabeth Deshko <elizabethrb1999@gmail.com>
2023-01-17 13:20:34 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import {useState} from "react";
+import {data} from "./data";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App () {
+
+    const [location, setLocation] = useState(0);
+
+    const {id, name, description, image} = data[location];
+
+    const previousLocation = () => {
+        setLocation((location) => {
+            location --;
+            if (location < 0){
+                return data.length -1;
+            }
+            return location;
+        })
+    }
+
+    const nextLocation = () => {
+        setLocation((location) => {
+            location ++;
+            if (location > data.length -1){
+                location =0;
+            }
+            return location;
+        })
+    }
+
+    return(<div>
+        <div className="container">
+            <img src={image} width="600px" alt="cities"/>
+        </div>
+        <div className="container">
+            <h1>{id}- {name}</h1>
+        </div>
+        <div className="container">
+            <h2>{description}</h2>
+        </div>
+        <div className="btn container">
+            <button onClick={previousLocation}>Previous</button>
+            <button onClick={nextLocation}>Next</button>
+        </div>
     </div>
-  );
+    )
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,59 @@
+export const data = [
+
+    {
+  
+      id: 1,
+  
+      name: "Split, Croatia",
+  
+      description: "Split is the second-largest city of Croatia, the largest city in Dalmatia and the largest city on the Croatian coast.",
+  
+      image: "https://qtxasset.com/quartz/qcloud1/media/image/luxurytraveladvisor/1583241741/Split%20Croatia%20waterfront%20and%20Marjan%20hill%20view%20-%20Getty-rev.jpg?VersionId=inMMgGmXiiM2SkEqzR8mkMocG8UMcSir"
+    },
+  
+    {
+  
+      id: 2,
+  
+      name: "Dubrovnik, Croatia",
+  
+      description: "Dubrovnik is a city in southern Croatia fronting the Adriatic Sea. It's known for its distinctive Old Town.",
+  
+      image: "https://assets.vogue.com/photos/59778a099c4d37358f9855a6/master/pass/00-tout-dubrovnik-croatia-travel-guide.jpg"
+    },
+  
+    {
+  
+      id: 3,
+  
+      name: "Lake Como, Italy",
+  
+      description: "Lake Como, in Northern Italy’s Lombardy region, is an upscale resort area known for its dramatic scenery, set against the foothills of the Alps.",
+  
+      image: "https://images.blacktomato.com/2022/05/Lake-Como-Inspirations.jpg?fm=pjpg&ixlib=php-3.3.1&s=41193471fcfe93d6eabededf56bdd523"
+  
+    },
+  
+    {
+  
+      id: 4,
+  
+      name: "Saint Petersburg, Russia",
+  
+      description: "Saint Petersburg, formerly known as Petrograd (1914–1924) and later Leningrad (1924–1991), is the second-largest city in Russia.",
+  
+      image: "https://www.worldatlas.com/upload/2b/c1/59/shutterstock-1407744842.jpg"
+    },
+  
+    {
+  
+      id: 5,
+  
+      name: "Prague, Czech Republic ",
+  
+      description: "Prague, capital city of the Czech Republic, is bisected by the Vltava River. Nicknamed 'the City of a Hundred Spires'.",
+  
+      image: "https://upload.wikimedia.org/wikipedia/commons/a/a7/Prague_%286365119737%29.jpg"
+    }
+  
+  ]
